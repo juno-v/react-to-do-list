14c671cb40eba75fc1db5363c8775932df6557df
Juno Vue <Juno.Vue@Hotmail.com>
2020-02-26 00:19:26 -0600

    added comments for engineers to read and learn

--- src/ToDoList/ToDoList.js
+++ src/ToDoList/ToDoList.js
@@ -1,37 +1,35 @@
 import React, { Component } from 'react';
 
 // import style sheet to use
+// use ./ because you have to go back out of the ToDoList.js file to get to ToDoList.css file
 import "./ToDoList.css"
 
-class App extends Component {
+class App extends Component { // this is a class component, which is different from a functional components. class components have state, as seen in next. 
 
   // what is state? 
-  // its an object 
+  // its an object that is meant to be changed, it doesn't stay the same all the time. 
+  // everytime state changes, the component will re render (component is the file. this is a component. ToDoList.js is a file)
   state = {
-    task: '', // set it to an empty string, this will become each individual task we put in
+    task: '', // set it to an empty string, this empty string will become each individual task we insert into the list
     taskList: [], // set to empty array, we will be adding tasks into this array 
   }
 
-  // changing state without mutating 
-  // explain immutable? 
-  // it takes an event 
-  // explain what a parameter is
-  // explain what an event is 
+  // here is a function that changes the state whenever it's called. 
+  // it takes an event, when something happens. like a bbq in july, an event. 
   handleTaskChange = (event) => {
     console.log(`You're entering in a value!`, event.target.value); // explain where event.target.value comes from  
     // if you want to clear console, just refresh at anytime. 
     this.setState({
-      ...this.state.task, // explain spread operator 
-      task: event.target.value, 
+      ...this.state.task, // the spread operator (...) allows you to add something new to the list, and retain the old ones. 
+      task: event.target.value, // setting the state of task (line 13) to become the event value that comes from this function. 
     })
   }
 
-  // ES6 function arrow function 
+  // ES6 function arrow function, research ES5 syntax if you'd like to learn more, but we'll be using ES6 javascript 
   // you can name functions whatever you want it to be, 
   // just make sure the name makes sense
   addTask = (event) => {
-    // a form submission automatically refreshes, we don't want that to happen
-    // or we will lose the data
+    // a form submission automatically refreshes, we don't want that to happen or we will lose data once it refreshes! 
     event.preventDefault(); 
     // test functionality with console.logs to see if it works 
     // must open DOM console to see it
@@ -40,18 +38,17 @@ class App extends Component {
     // and you are writing the onClick event 
     console.log(`add task button clicked!`);
 
-    // putting the task into the task list array 
+    // putting the new task into the task list array state (line 14)
     this.state.taskList.push(this.state.task); 
 
     // you can see your new state now
     console.log(`the task.`, this.state.task);
     console.log(`the task LIST.`, this.state.taskList);
     
-    // clear state 
+    // clear task state  
     this.setState({
-      task: '', // set it back to an empty string
+      task: '', // set task back to an empty string, but it's in the array. this is so that the input field will "refresh" back to empty so we can write new tasks. 
     })
-    
   }
 
   deleteTask = (event) => {
@@ -60,27 +57,35 @@ class App extends Component {
 
 
 
-    // event.target.value is the index 
-    // the 1 means remove 1 element 
+    // event.target.value is the index in this case since we've assigned it that way (line 79)
     // together, it's saying remove 1 element from the index, essentially the index itself. 
-    this.state.taskList.splice(event.target.value, 1); 
-    this.setState({
+    this.state.taskList.splice(event.target.value, 1);    // the 1 means remove 1 element, what happens if we remove the 1? 
+    console.log(`what is taskList?`, this.state.taskList)
+    this.setState({ // we are setting the taskList state her to become what is left after deleting a task,
+      // we don't want the array to still have the deleted ask, once it's been deleted. so we update task list state array 
       taskList: this.state.taskList, 
     })
   }
 
+  test = (event) => {
+    console.log(`testing something this.state.taskList`, this.state.taskList); 
+
+    // console.logs are great to see what you are aiming to look at. so if you aren't sure what's happening where, whether it be a function or a variable or anything, just console.log
+    // to see if you see what you're looking for, if it's not there, something's wrong! always check typos first... 
+  }
+
   render() {
 
-    let tasks = this.state.taskList;
-    let taskList = tasks.map((task, index)=> {
+    let tasks = this.state.taskList; // initlize variable to hold our taskList array 
+    let taskList = tasks.map((task, index)=> { // map out taskList, since it's an array, we can use .map and return individual iterations of each index 
       return (
-        <div className="taskDiv" key={index}> 
-            <li>
-              {task}
+        <div className="taskDiv" key={index}> {/* we need a unique key, indexes are unique since it's a particular digit space, use that. */}
+            <li>                               {/* pay attention to your console, if the it keeps saying the "key" message try different elements within the .map */}
+              {task} {/* in line 80 we alias each index of the tasks to be task, so we return the alias in one object since it's iterating. */}
               <button
               className="deleteTaskButton"
               value={index}
-              onClick={this.deleteTask}
+              onClick={this.deleteTask} /* with the onClick event, we are calling the deleteTask function from line 54 */
               > Delete </button>
             </li>
         </div> 
@@ -93,29 +98,31 @@ class App extends Component {
             <div className="listDiv">
 
             <ol>
-              {taskList}
+              {taskList} {/* we've mapped out taskList in 79-93, so we should we seeing one div, with a task and a delete button for everything task, in taskList */}
             </ol>
 
-            <form onSubmit={this.addTask}>
+            <form onSubmit={this.addTask}> 
               <label>
-              {/* make the value state, explain why */}
-              <input 
-              className="taskInputField"
-              onChange={this.handleTaskChange}
-              type="text"
-              value={this.state.task}/> 
+                <input 
+                className="taskInputField"
+                onChange={this.handleTaskChange} /* this onChange event calls a function anytime it changes, open your console and type, what do you see?  */
+                type="text"
+                value={this.state.task}/> {/* the value of the text field will be task, once we click enter, the value will change task state, and go into task list state */}
               </label>
             </form>
 
-            {/* have to write this.function name to refer to the function in this component */}
             <button 
             onClick={this.addTask}
             className="addTaskButton"
             > Add Task </button>
 
             <br/> 
-            {/* test to see something */}
-            {/* {JSON.stringify(this.state.taskList)} */}
+            <button onClick={this.test}> Test </button>
+
+            <br/> 
+            {/* test to see something on the screen, sometimes I use this instead of console.log, but it servres the same purpose. uncomment, what do you see? */}
+            {/* {JSON.stringify(this.state.taskList)}
+            {JSON.stringify(this.state.task)} */}
             </div>
       </div>
     );
